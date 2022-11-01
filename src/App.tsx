6c7cf9747e7f1f585a605d3df51aa2b025e915f2
jaewoogwak <jaewoo010207@gmail.com>
2022-11-02 00:16:01 +0900

    feat: 프로젝트 초기 설정 #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import DefaultLayout from "./Layout";
+import Home from "./pages/Home/Home";
+import Management from "./pages/Management/Management";
 
 function App() {
-  return <div className="App">MainPage</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/manager" element={<Management />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
